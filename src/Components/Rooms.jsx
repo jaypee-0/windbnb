9add681839ee362549e3a949a502e5f5fb99783c
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-05-09 12:01:01 +0100

    First Commit

--- src/Components/Rooms.jsx
+++ src/Components/Rooms.jsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import data from '../data/stays.json'
 import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome'
 
 const Rooms = () => {
 
-const [loading, setloading] = useState(true)
-//     
+//const [loading, setloading] = useState(true)    
 
   return (
     <div className='container'>
         <div className='d-flex my-3 justify-content-between'>
-            <header>Stays in Finland</header>
-            <p>12+ stays</p>
+            <header className='fs-2 fw-bolder d-gray'>Stays in Finland</header>
+            <p className='mr-5 gray-3 fs-5'>14+ stays</p>
         </div>
         <div className='d-flex w-100 flex-wrap mx-auto'>
         {data.map(data => {
@@ -20,14 +19,14 @@ const [loading, setloading] = useState(true)
                     <img className='img-fluid mb-2 w-90' src={data.photo} alt="" />
                     <div className='d-flex w-90 justify-content-between'>
                         <div className='d-flex align-items-center'>
-                        {data.superHost && <p className='border border-2 rounded-pill py-1 fw-bold px-3'>SUPER HOST</p>}
-                        <p className="type">{data.type} </p>
-                        {data.beds && <p><span className='mr-1'>.</span> {data.beds} beds</p>}
+                            {data.superHost && <p className='super-post mr-2 py-1 fw-bold px-3'>SUPER HOST</p>}
+                            <p className="type">{data.type} </p>
+                            {data.beds && <p className='gray-3 fw-bold beds'><span className='mr-1'>.</span> {data.beds} beds</p>}
                         </div>
                         <p className="rating"><FA className='mr-1' icon="fa-star" />{data.rating}</p>
                     </div>                      
-                    <p className="title">{data.title}</p>
-                </div>
+                    <p className="title fw-bold ">{data.title}</p>
+                </div>  
             )
         })}
         </div>
