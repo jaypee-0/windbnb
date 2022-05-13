8163389c10c74a203c6032f23b7ed5f26724f985
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-05-13 13:47:56 +0100

    Updated counter

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,27 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../Assets/logo.png'
 import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome'
 import data from '../data/stays.json'
-const Nav = () => { 
-    
+import { useCallback } from 'react'
+
+const Nav = () => {     
     const [showtop, setshowtop] = useState(false)
     const [showlocation, setshowlocation] = useState(false)
     const [showguests, setshowguests] = useState(false)
-    const [increment, setincrement] = useState('')
+    const [incrementa, setincrementa] = useState(0)
+    const [incrementb, setincrementb] = useState(0)
+    const [incredecre, setincredecre] = useState(0)    
 
     const handleTopLocate = () => {
         setshowtop(true)
         setshowlocation(true)  
         setshowguests(false)        
     }
+    const additiona = () => {
+        setincrementa(count => count + 1);
+    };
+    const subtractiona = () => {
+        if (incrementa > 0 ) {
+            setincrementa(count => count - 1);
+        }
+    }
+    const additionb = () => {
+        setincrementb(count => count + 1);      
+    };
+    const subtractionb = () => {
+        if (incrementb > 0 ) {
+            setincrementb(count => count - 1);
+        }
+    }
     const handleTopGuest = () => {
         setshowtop(true)
-        setshowguests(true)      
-        setincrement( {
-            // if (increment = 0, increment ++1 ) {
-                
-            // }
-        })      
+        setshowguests(true)    
     }
     const locationlists = new Map(
         data.map((data) => [data.id, data.city])
@@ -33,7 +47,13 @@ const Nav = () => {
         setshowlocation(false)
         setshowguests(false)
     }
-
+    const handleguests = useCallback(() => {
+        setincredecre(incrementa + incrementb);
+      }, [incrementa, incrementb])
+    useEffect(() => {
+      handleguests()
+    }, [handleguests]);
+     
   return (
     <div>
         <nav className='container-md position-relative'>
@@ -44,17 +64,17 @@ const Nav = () => {
                 <form className={showtop ? 'openMenu active' : 'null'}>                 
                 <div className='form d-flex mx-auto mt-3 mt-md-0 mx-md-0'>
                     {showtop ?
-                        <select className='custom-select w-100 py-2 py-md-3 pl-2'><FA className='pr-3 index3' icon="fa-location-dot" />
+                        <select className='custom-select w-100 py-1 py-md-3 pl-2'><FA className='pr-3 index3' icon="fa-location-dot" />
                         <option style={{ padding: 20, border: 0 }} value='0'><FA className='ml-3' style={{color: 'red'}} icon="fa-location-dot" />Location</option>
                             {[...locations].map(([id, city]) => (
-                            <option style={{ padding: 20, border: 0 }} className='py-2 border-0' key={id} value={id}><FA className='ml-3 index3' icon="fa-location-dot" />{city}</option>
+                            <option style={{ padding: 20, border: 0 }} className='py-1 border-0' key={id} value={id}><FA className='ml-3 index3' icon="fa-location-dot" />{city}</option>
                             ))}
                         </select> : 
                         <input type="text" onClick={handleTopLocate} className='w-100 input1 py-2 py-md-3 pl-2' placeholder='Place'/>
                     
                     }
-                    <input type="text" onClick={handleTopGuest} value={showguests ? 'guests' : ''} className='w-100 input2 py-2 py-md-3 pl-2' placeholder='Add guests'/>
-                    <button onClick={handleTopButt} className={showtop ? 'd-flex bigbutt my-auto' : 'button py-2 py-md-3 px-3'}>{<FA icon={showtop ? "fa-search pl-2 pr-4" : "fa-search"} />}{showtop ? 'Search' : ''}</button>
+                    <input type="text" onClick={showguests ? '' : handleTopGuest} value={showguests ? `${incredecre}  guests` : ''} className='w-100 input2 py-2 py-md-3 pl-2' placeholder='Add guests'/>
+                    <button onClick={handleTopButt} className={showtop ? 'd-flex bigbutt my-auto py-2 py-md-3' : 'button py-2 py-md-3 px-3'}>{<FA icon={showtop ? "fa-search pl-2 pr-4" : "fa-search"} />}{showtop ? 'Search' : ''}</button>
                 </div> 
                 </form>              
             </div>
@@ -62,21 +82,28 @@ const Nav = () => {
             <FA onClick={closeTop} className="position-absolute" icon={showtop ? 'fa-xmark' : 'd-none'} />
             </div>
             }
-            <div className="d-flex flex-row navdeets col-12">
+            <div className="d-flex flex-row navdeets justify-content-end col-11 col-md-12">
             {showlocation && <div className='col-6'>
                     
             </div>
             }
             {showguests && <div className='col-6'>
-                <div>
-                    <h3>Adults</h3>
+                <div className='showguests mt-2'>
+                    <h4>Adults</h4>
                     <p>Ages 13 or above</p>
-                    <div></div>
+                    <div className='d-flex flex-row'>
+                    <button onClick={subtractiona} className='buttons px-2'>-</button>
+                    <p className='px-3 mb-0'>{incrementa}</p>
+                    <button onClick={additiona} className='buttons'>+</button>
+                    </div>
                 </div>
-                <div>
-                    <h3>Children</h3>
+                <div className='showguests mt-2'>
+                    <h4>Children</h4>
                     <p>Ages 2 - 12</p>
-
+                    <div className='d-flex flex-row'>
+                    <button onClick={subtractionb} className='buttons px-2'>-</button>
+                    <p className='px-3 mb-0'>{incrementb}</p>
+                    <button onClick={additionb} className='buttons'>+</button></div>            
                 </div>
             </div>}
             </div>
